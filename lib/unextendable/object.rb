ba04d9b818f717648c1bec4373923026830ed59e
Paul Engel <paul.engel@holder.nl>
2011-05-02 23:43:43 +0200

    Added object.meta_class? (aliased with singleton_class?)

--- lib/unextendable/object.rb
+++ lib/unextendable/object.rb
@@ -8,6 +8,11 @@ class Object
   end
   alias :singleton_class :meta_class
 
+  def meta_class?
+    meta_class rescue false
+  end
+  alias :singleton_class? :meta_class?
+
   meta_class do
     def extended_modules
       @extended_modules ||= []
